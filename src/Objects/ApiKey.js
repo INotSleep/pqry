0639fabe12cea8b825ed25db53c71bc4f258c69b
INotSleep <77581131+INotSleep@users.noreply.github.com>
2022-03-12 13:26:51 +0200

    Uploaded ApiKey class

---
+++ src/Objects/ApiKey.js
@@ -0,0 +1,21 @@
+import { DeleteApiKey } from "./../Commands/DeleteApiKey.js";
+
+class ApiKey {
+	constructor ({identifier, description, allowed_ips, last_used_at, created_at, host, apikey}) {
+		this.identifier = identifier
+		this.description = description;
+		this.allowed_ips = allowed_ips;
+		this.last_used_at = last_used_at;
+		this.created_at = created_at;
+		this.host = host;
+		this.apikey = apikey;
+	
+		this.delete = async function () {
+			return await DeleteApiKey(this.host, this.apikey, this.identifier)
+	    }
+	};
+};
+
+export {
+	ApiKey
+};
